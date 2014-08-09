0d25e4613b7417114cbd66c4baf3e367127db176
jtong <tj19832@gmail.com>
2014-08-09 22:13:08 +0800

    jtong - add practise 5 to section 2 and add specs of 4,5 in section2

--- codes/specs/section_2/practice_5.js
+++ codes/specs/section_2/practice_5.js
@@ -12,13 +12,18 @@ describe('practice-2-1', function () {
     it("use star selector to change all elements", function (done) {
         console.log(JSON.stringify(css_object));
 
-        expect(css_object.stylesheet.rules[0].selectors[0]).toBe('.abstract');
+        expect(css_object.stylesheet.rules[0].selectors[0]).toBe('#w3c-link');
 
         var border_values = _(css_object.stylesheet.rules[0].declarations).findWhere({property:"border"}).value.split(" ");
         expect(border_values).toContain("thin");
         expect(border_values).toContain("black");
         expect(border_values).toContain("solid");
 
+        var color = _(css_object.stylesheet.rules[0].declarations).findWhere({property:"color"});
+        expect(color).toBe("black");
+
+        var text_decoration = _(css_object.stylesheet.rules[0].declarations).findWhere({property:"text-decoration"});
+        expect(text_decoration).toBe("none");
         done();
     });
 });
