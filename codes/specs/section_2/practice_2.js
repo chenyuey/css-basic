4cfc5fafb0e9d77a170ed45ef8456509ecfe1a61
jtong <tj19832@gmail.com>
2014-08-09 17:32:12 +0800

    jtong - add new practise for section 2

--- codes/specs/section_2/practice_2.js
+++ codes/specs/section_2/practice_2.js
@@ -10,16 +10,14 @@ describe('practice-2-1', function () {
     });
 
     it("use star selector to change all elements", function (done) {
+        console.log(JSON.stringify(css_object));
 
-        expect(css_object.stylesheet.rules[0].selectors[0]).toBe('*');
+        expect(css_object.stylesheet.rules[0].selectors[0]).toBe('article p');
 
         var border_values = _(css_object.stylesheet.rules[0].declarations).findWhere({property:"border"}).value.split(" ");
-        console.log(border_values);
-
         expect(border_values).toContain("thin");
         expect(border_values).toContain("black");
         expect(border_values).toContain("solid");
-
 
         done();
     });
