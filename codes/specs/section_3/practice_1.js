2f8f4ba40d38f10325793813c1af5b8465015663
jtong <tj19832@gmail.com>
2014-08-10 14:24:39 +0800

    jtong - add css inherit practise, move practise 1 to practise 2, all for section 3

--- codes/specs/section_3/practice_1.js
+++ codes/specs/section_3/practice_1.js
@@ -10,13 +10,36 @@ describe('practice_1_1', function () {
     });
 
     it("id's priority higher than element selector", function (done) {
+        var rules = css_object.stylesheet.rules;
 
-            console.log(JSON.stringify(css_object));
-            expect(css_object.stylesheet.rules[0].selectors[0]).toBe('#change');
-            expect(css_object.stylesheet.rules[0].declarations[0].property).toBe('color');
-            expect(css_object.stylesheet.rules[0].declarations[0].value).toBe('black');
-            done();
+        var abstract_rule = _(rules).find(function(rule){
+            return _(rule.selectors).contains(".abstract");
         });
+
+        expect(abstract_rule.selectors[0]).toBe('.abstract');
+        expect(abstract_rule.declarations[0].property).toBe("color");
+        expect(abstract_rule.declarations[0].value).toBe("grey");
+
+        var abstract_a_rule = _(rules).find(function(rule){
+            return rule.selectors.length ==1 && _(rule.selectors).contains(".abstract a");
+        });
+
+        expect(abstract_a_rule.selectors[0]).toBe('.abstract a');
+
+        var abstract_a_rule_color_value = _(abstract_a_rule.declarations).findWhere({property:"color"}).value;
+        expect(abstract_a_rule_color_value).toBe('inherit');
+
+
+        var abstract_a_rule_text_decoration_value = _(abstract_a_rule.declarations).findWhere({property:"text-decoration"}).value
+        expect(abstract_a_rule_text_decoration_value).toBe('none');
+
+        var border_values = _(abstract_a_rule.declarations).findWhere({property:"border"}).value.split(" ");
+        expect(border_values).toContain("thin");
+        expect(border_values).toContain("black");
+        expect(border_values).toContain("solid");
+
+        done();
+    });
 });
 
 
